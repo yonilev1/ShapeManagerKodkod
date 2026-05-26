5e8d3346aa2215589be1624751599b268bd6f15c
Yoni <yonilev2021@gmail.com>
2026-05-26 17:45:29 +0300

    added info logs for methods

--- square.py
+++ square.py
@@ -14,8 +14,10 @@ class Square(shape.Shape):
         length(int/float): the length of every Side
         logger(logging): the logger
        """
-       super().__init__(shape_id, shape_type, logger)  
+       super().__init__(shape_id, shape_type, logger)
+       self.logger.info("sent shape_id, shape_type, logger to super() in base class")
        self.length = length
+       self.logger.info(f"finished in init in {shape_type}")
  
     def get_area(self):
        """
@@ -24,6 +26,7 @@ class Square(shape.Shape):
        Returns:
         int/float: area of the square
        """
+       self.logger.info(f"in function get_area to get area of {self.shape_type}")
        return self.length * self.length / 2 
  
     def get_perimeter(self):
@@ -33,6 +36,7 @@ class Square(shape.Shape):
        Returns:
             int/float: the perimeter
        """
+       self.logger.info(f"in function get_perimeter to get perimeter of {self.shape_type}")
        return 4 * self.length 
  
     def to_dict(self): 
@@ -40,6 +44,7 @@ class Square(shape.Shape):
        Returns:
         a dict with the data of the shape to store in the DB
        """
+       self.logger.info(f"in function to_dict to get dict of {self.shape_type}")
        return {'id':self.shape_id, 'type':self.shape_type, 'side':self.length} 
 
  
